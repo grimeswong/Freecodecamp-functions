435d1ee11a45d400d2a03ab3bd7b9c856c9e3128
Shadow Grimes <grimeswong@gmail.com>
2018-02-04 22:45:26 +1000

    Setup basic style structure for Sum All number component

--- src/components/SumAllNumberInRange/index.js
+++ src/components/SumAllNumberInRange/index.js
@@ -4,13 +4,19 @@ import sumAllNumberInRange from "../../utils/sumAllNumberInRange.js";
 const SumAllNumberInRange = () => {
 
   return (
-    <div>
-      <h2>Sum All Number In Range</h2>
-      <p>Return the sum of those two numbers and all numbers between them (two numbers (lowest numbers can come last)). </p>
-      <p>sumAllNumberInRange([1, 4])</p>
-      <h3>Answer = {sumAllNumberInRange([1, 4])}</h3>
-      <h3>Source Code: </h3>
-      <p>{sumAllNumberInRange.toString()}</p>
+    <div className="component-wrapper-top">
+      <div className="function-upper-wrapper">
+      <h2 className="function-title">Sum All Number In Range</h2>
+      <p className="function-description">Return the sum of those two numbers and all numbers between them (two numbers - lowest numbers can come last). </p>
+      </div>
+      <div className="function-lower-wrapper">
+      <p className="function-argument">sumAllNumberInRange([1, 4])</p>
+      <h3 className="function-result">Answer = {sumAllNumberInRange([1, 4])}</h3>
+      </div>
+      <div className="function-sourcode-wrapper">
+        <h3 className="function-sourcecode-title">Source Code: </h3>
+        <p className="function-sourcecode-source">{sumAllNumberInRange.toString()}</p>
+      </div>
     </div>
   );
 };
