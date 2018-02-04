12b180b694aa264b5ca1226be04a9a4b7468c830
Shadow Grimes <grimeswong@gmail.com>
2018-02-04 21:29:05 +1000

    Refactor the DiffTwoArrays component that into Utils  folder

--- src/components/DiffTwoArrays/index.js
+++ src/components/DiffTwoArrays/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
+import diffTwoArrays from '../../utils/diffTwoArrays.js';
 
 const DiffTwoArrays = () => {
 
-  function diffArray(arr1, arr2) {
-    var newArr = [];
-    var result = [];
-    newArr = arr1.concat(arr2).sort();
-
-    while(newArr.length != 0) {
-      if(newArr[0] == newArr[1]) {
-        newArr = newArr.slice(2);
-      } else {
-        result.push(newArr[0]);
-        newArr = newArr.slice(1);
-      }
-    }
-    return result;
-  }
-
     return (
       <div>
-        <h2>Difference of two Arrays</h2>
+        <h2>Difference of Two Arrays</h2>
         <p> Array 1 = ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"] </p>
         <p> Array 2 = ["diorite", "andesite", "grass", "dirt", "dead shrub"] </p>
-        <h3>Answer = {diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])}</h3>
-        <h3>Source Code: </h3> <p>{diffArray.toString()}</p>
+        <h3>Answer = {diffTwoArrays(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])}</h3>
+        <h3>Source Code: </h3> <p>{diffTwoArrays.toString()}</p>
       </div>
     );
 }
---
+++ src/utils/diffTwoArrays.js
@@ -0,0 +1,21 @@
+/*
+  Return the difference of two arrays' element(s) 
+*/
+
+function diffTwoArray(arr1, arr2) {
+  var newArr = [];
+  var result = [];
+  newArr = arr1.concat(arr2).sort();
+
+  while(newArr.length != 0) {
+    if(newArr[0] == newArr[1]) {
+      newArr = newArr.slice(2);
+    } else {
+      result.push(newArr[0]);
+      newArr = newArr.slice(1);
+    }
+  }
+  return result;
+}
+
+export default diffTwoArray;
