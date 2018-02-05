d141b794074d7f518dde8d4ab501a99a6c137039
Shadow Grimes <grimeswong@gmail.com>
2018-02-06 09:48:46 +1000

    Updated getGeolocation function from jQuery to javascript

--- src/utils/getGeolocation.js
+++ src/utils/getGeolocation.js
@@ -1,10 +1,19 @@
 /*
     Get current user geolocation and return the latitude and longitude
 */
+function getGeolocation() {
 
+  if(navigator.geolocation) {
+    console.log(navigator.geolocation);
+    navigator.geolocation.getCurrentPosition(function(position) {
+    var locationMessage = "latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude;
+    console.log(locationMessage);
+      return locationMessage;
+    });
+  } else {
+    return "Couldn't get user location details";
+  }
 
-if(navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-  });
 }
+
+export default getGeolocation;
