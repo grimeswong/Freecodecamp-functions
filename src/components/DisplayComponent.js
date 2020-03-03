c04d8a7cb26d221273cedcc3db917f10c8bcc03a
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-03 10:50:56 +1000

    Refine the Display component that load all functions from a list of imports

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Data from '../data/data.js'
-import caesarCipher from '../utils/caesarCipher.js';
+import * as util from '../utils/utilList.js'; // loaded all the functions from the utilities list
 
 const DisplayComponent = () => {
   console.log(`Data is = ${Data[0].title}`);
@@ -14,11 +14,11 @@ const DisplayComponent = () => {
       <div className="function-lower-wrapper">
         <p className="function-test-title"></p>
         <p className="function-argument">{Data[0].test.argument}</p>
-        <p className="function-result">{caesarCipher("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.")}</p>
+        <p className="function-result">{util.caesarCipher("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.")}</p>
       </div>
       <div className="function-sourcecode-wrapper">
         <p className="function-sourcecode-title"></p>
-        <p className="function-sourcecode-source">{caesarCipher.toString()}</p>
+        <p className="function-sourcecode-source">{util.caesarCipher.toString()}</p>
       </div>
     </div>
   )
---
+++ src/utils/utilList.js
@@ -0,0 +1,39 @@
+import diffTwoArrays from './diffTwoArrays.js';
+import sumAllNumberInRange from './sumAllNumberInRange.js';
+import romanNumeralConverter from './romanNumeralConverter.js';
+import getGeolocation from './getGeolocation.js';
+import caesarCipher from './caesarCipher.js';
+import whereDoIBelong from './whereDoIBelong.js';
+import seekAndDestroy from './seekAndDestroy.js';
+import falsyBouncer from './falsyBouncer.js';
+import mutations from './mutations.js';
+import repeatAString from './repeatAString.js';
+import chunkyMonkey from './chunkyMonkey.js';
+import confirmTheEnding from './confirmTheEnding.js';
+import largestNumbersInArrays from './largestNumbersInArrays.js';
+import titleCaseASentence from './titleCaseASentence.js';
+import findTheLongestWordInAString from './findTheLongestWordInAString.js';
+import checkForPalindromes from './checkForPalindromes.js';
+import whereforeArtThou from './whereForeArtThou.js';
+import searchAndReplace from './searchAndReplace.js';
+import truncateAString from './truncateAString.js';
+
+export {diffTwoArrays}
+export {sumAllNumberInRange}
+export {romanNumeralConverter}
+export {getGeolocation}
+export {caesarCipher}
+export {whereDoIBelong}
+export {seekAndDestroy}
+export {falsyBouncer}
+export {mutations}
+export {repeatAString}
+export {chunkyMonkey}
+export {confirmTheEnding}
+export {largestNumbersInArrays}
+export {titleCaseASentence}
+export {findTheLongestWordInAString}
+export {checkForPalindromes}
+export {whereforeArtThou}
+export {searchAndReplace}
+export {truncateAString}
