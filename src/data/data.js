f56da843fef04dd0962f85fb74cd42bc8fbb975c
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-04 00:30:33 +1000

    Add truncate a string

--- src/data/data.js
+++ src/data/data.js
@@ -153,6 +153,15 @@ const Data = [
       argument: ["I'm a little tea pot"],
     }
   },
+  {
+    title:"Truncate A String",
+    funcName: "truncateAString",
+    description: "Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending. /n Note that inserting the three dots to the end will add to the string length.\n However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string length in determining the truncated string.",
+    argument: ["(\"Absolutely Longer\", 2)"],
+    test: {
+      argument: ["Absolutely Longer", 2],
+    }
+  },
 ]
 
 export default Data;
