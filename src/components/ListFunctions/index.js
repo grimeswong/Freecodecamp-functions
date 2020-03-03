ef8b3bb81750eed3b852b58723fc3277810f8bcc
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-03 23:05:45 +1000

    Get the total of functions number and refine styling

--- src/components/ListFunctions/index.js
+++ src/components/ListFunctions/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Data from '../../data/data.js';
 
 const ListFunctions = (props) => {
-
+  console.log(Data.length)
   return(
     <div>
-      <ul>
+      <ul>List of JS function ({Data.length})
         {Data.map((utility, index) => {
           return <li onClick={()=>props.onFunctionSelect(utility.title)} key={index}>{utility.title}</li>
         })}
