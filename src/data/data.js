0b37ebd9bddaa3ea6baeb9e90e7134eb18ef3165
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-03 23:47:05 +1000

    Add largest numbers in arrays

--- src/data/data.js
+++ src/data/data.js
@@ -10,7 +10,7 @@
   // {
   //   title:"",
   //   funcName: "",
-  //   description:"",
+  //   description: "",
   //   argument: [],
   //   test: {
   //     argument: [],
@@ -21,7 +21,7 @@ const Data = [
   {
     title:"Difference of Two Arrays",
     funcName: "diffTwoArrays",
-    description:"Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both.",
+    description: "Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both.",
     argument: [["Array 1 = [\"diorite\", \"andesite\", \"grass\", \"dirt\", \"pink wool\", \"dead shrub\"]"],["Array 2 = [\"diorite\", \"andesite\", \"grass\", \"dirt\", \"dead shrub\"]"]],
     test: {
       argument: [["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]]
@@ -30,7 +30,7 @@ const Data = [
   {
     title: "Caesar Ciper",
     funcName: "caesarCipher",
-    description:  "function which shifts 13 ciphers to encode string as input and returns a decoded string.",
+    description: "function which shifts 13 ciphers to encode string as input and returns a decoded string.",
     argument: ["(\"GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.\")"],
     test: {
       argument: ["GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK."]
@@ -39,7 +39,7 @@ const Data = [
   {
     title:"Check For Palindromes",
     funcName: "checkForPalindromes",
-    description:"A palindrome is a word or sentence that's spelled the same way both forward",
+    description: "A palindrome is a word or sentence that's spelled the same way both forward",
     argument: ["(\"A man, a plan, a canal. Panama\")"],
     test: {
       argument: ["A man, a plan, a canal. Panama"]
@@ -48,7 +48,7 @@ const Data = [
   {
     title:"Chunky Monkey",
     funcName: "chunkyMonkey",
-    description:"A function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.",
+    description: "A function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.",
     argument: ["([\"a\", \"b\", \"c\", \"d\"], 2)"],
     test: {
       argument: [["a", "b", "c", "d"], 2],
@@ -57,7 +57,7 @@ const Data = [
   {
     title:"Confirm The Ending",
     funcName: "confirmTheEnding",
-    description:"Check if a string (first argument, str) ends with the given target string (second argument, target).",
+    description: "Check if a string (first argument, str) ends with the given target string (second argument, target).",
     argument: ["(\"If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing\", \"mountain\")"],
     test: {
       argument: ["If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain"],
@@ -66,7 +66,7 @@ const Data = [
   {
     title:"Falsy Bouncer",
     funcName: "falsyBouncer",
-    description:"Remove all falsy values from an array like false, null, 0, \"\", undefined, and NaN.",
+    description: "Remove all falsy values from an array like false, null, 0, \"\", undefined, and NaN.",
     argument: ["([1, null, NaN, 2, undefined])"],
     test: {
       argument: [[1, null, NaN, 2, undefined]],
@@ -75,10 +75,19 @@ const Data = [
   {
     title:"Find The Longest Word In A String",
     funcName: "findTheLongestWordInAString",
-    description:"Return the length of the longest word in the provided sentence.",
+    description: "Return the length of the longest word in the provided sentence.",
     argument: ["(\"What is the average airspeed velocity of an unladen swallow\")"],
     test: {
       argument: ["What is the average airspeed velocity of an unladen swallow"],
+    }
+  },
+  {
+    title:"Largest Numbers In Arrays",
+    funcName: "largestNumbersInArrays",
+    description:"Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.",
+    argument: ["[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]"],
+    test: {
+      argument: [[[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]],
     }
   },
 ]
